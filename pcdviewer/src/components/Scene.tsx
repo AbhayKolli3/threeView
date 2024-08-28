d929ad5aa28cafd24253012b3dd146a8d047014e
AbhayKolli3 <abhay.kolli@telusinternational.com>
2024-08-28 14:05:48 +0530

    initial commit

---
+++ pcdviewer/src/components/Scene.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+//@ts-ignore
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+//@ts-ignore
+import { PCDLoader } from 'three/addons/loaders/PCDLoader.js';
+
+export default function Scene() {
+    
+    const [idx,setIdx]=useState(0)
+     
+    const urls = ['https://raw.githubusercontent.com/mrdoob/three.js/4b18dbe78bec6067cd98e66539efe1b157f5635f/examples/models/pcd/ascii/simple.pcd','https://segmentsai-dev.s3.eu-west-2.amazonaws.com/assets/tobias-admin/c92c069f-94b5-4b72-859b-08a7a7a141a4.pcd'];
+    
+    function changePCD(){
+        
+        setIdx(idx+1)
+        console.log(idx)
+    }
+
+    useEffect(()=>{
+        
+
+        function setupScene(){
+            const scene= new THREE.Scene()
+            return scene
+        }
+
+        function setupRenderer(){
+            const renderer = new THREE.WebGLRenderer( { antialias: true } );
+            renderer.setPixelRatio( window.devicePixelRatio );
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            document.body.appendChild( renderer.domElement );
+            return renderer
+        }
+
+        function setupCamera(){
+            const camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.01, 40 );
+            camera.position.set( 0, 0, 1 );
+            scene.add( camera );
+            return camera
+        }
+        function render(){
+            renderer.render(scene,camera)
+        }
+
+        function setupControls(){
+            const controls = new OrbitControls( camera, renderer.domElement );
+				controls.addEventListener( 'change', render ); // use if there is no animation loop
+				controls.minDistance = 0.5;
+				controls.maxDistance = 10;
+                return controls
+
+        }
+
+        function loadPCD(){
+            
+            const loader = new PCDLoader();
+            
+            
+
+            loader.load( urls[idx], function ( points:any ) {
+
+                console.log(points)
+
+                points.geometry.center();
+                points.geometry.rotateX( Math.PI );
+                points.name = 'icecake.pcd';
+                scene.add( points );
+                render();
+
+            } );
+            return loader
+
+
+        }
+
+
+        const renderer = setupRenderer()
+        const scene = setupScene()
+        const camera = setupCamera()
+        const controls = setupControls()
+        const loader= loadPCD()
+
+        render()
+
+
+
+})
+
+  return (
+  <div id="controls">
+    <button id="dec" onClick={changePCD}  >Prev</button>
+    <button id="inc" onClick={changePCD}>Next</button>
+
+  </div>
+  );
+}
